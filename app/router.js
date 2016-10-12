ebedf3ed389d77d66bd5dcbde1890fd8207f456f
Martin Kautz <kautz@ocean-insights.com>
2016-10-12 14:34:05 +0200

    Added U in CRUD. Cleanup.

--- app/router.js
+++ app/router.js
@@ -7,9 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('participants', function() {
-    this.route('participant', { path: ':id' });
-  });
+  this.route('participants');
 });
 
 export default Router;
